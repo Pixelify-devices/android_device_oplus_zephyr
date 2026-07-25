5ffc1225eba93d954bb49434bc2969b3644d2dd9
Thundergod_thor <adityasingh6901012a@gmail.com>
2026-07-25 13:28:13 +0000

    zephyr: patch audio.primary.mediatek against libalsautils-v31

--- extract-files.py
+++ extract-files.py
@@ -63,7 +63,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
+        .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
     ('vendor/lib64/mt6895/libcam.hal3a.so',
      'vendor/lib64/mt6895/libcam.hal3a.ctrl.so',
      'vendor/lib64/mt6895/libmtkcam_request_requlator.so',
